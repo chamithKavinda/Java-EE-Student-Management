7a6a05367bc702e8406a135848a656dfc78fa5f0
chamithKavinda <chamith13kavinda@gmail.com>
2024-07-14 12:14:45 +0530

    student get done

--- src/main/java/lk/ijse/gdse68/studentmanagement/Controller/Student.java
+++ src/main/java/lk/ijse/gdse68/studentmanagement/Controller/Student.java
@@ -28,6 +28,7 @@ import static lk.ijse.gdse68.studentmanagement.util.Util.idGenerate;
 public class Student extends HttpServlet {
     Connection connection;
     public static String SAVE_STUDENT = "INSERT INTO student (id,name,email,city,level) VALUES(?,?,?,?,?)";
+    public static String GET_STUDENT = "SELECT * FROM student WHERE id=?";
     @Override
     public void init() throws ServletException {
         try {
@@ -74,7 +75,26 @@ public class Student extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try(var writer = resp.getWriter()) {
+            StudentDTO studentDTO = new StudentDTO();
+            Jsonb jsonb = JsonbBuilder.create();
+            var studentId = req.getParameter("studentId");
+            var ps = connection.prepareStatement(GET_STUDENT);
+            ps.setString(1, studentId);
+            var rst = ps.executeQuery();
+            while (rst.next()){
+                studentDTO.setId(rst.getString("id"));
+                studentDTO.setName(rst.getString("name"));
+                studentDTO.setEmail(rst.getString("email"));
+                studentDTO.setCity(rst.getString("city"));
+                studentDTO.setLevel(rst.getString("level"));
+            }
+            resp.setContentType("application/json");
+            jsonb.toJson(studentDTO,writer);
 
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
